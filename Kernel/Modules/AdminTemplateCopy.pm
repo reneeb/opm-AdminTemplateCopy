bc1ab1e10307cb2aebeeb83b358a6b4c43e7519d
reneeb <info@perl-services.de>
2017-04-25 18:28:06 +0200

    initial commit

---
+++ Kernel/Modules/AdminTemplateCopy.pm
@@ -0,0 +1,90 @@
+# --
+# Copyright (C) 2017 Perl-Services.de, http://perl-services.de
+# --
+# This software comes with ABSOLUTELY NO WARRANTY. For details, see
+# the enclosed file COPYING for license information (AGPL). If you
+# did not receive this file, see http://www.gnu.org/licenses/agpl.txt.
+# --
+
+package Kernel::Modules::AdminTemplateCopy;
+
+use strict;
+use warnings;
+
+use Kernel::Language qw(Translatable);
+
+our $ObjectManagerDisabled = 1;
+
+sub new {
+    my ( $Type, %Param ) = @_;
+
+    # allocate new hash for object
+    my $Self = {%Param};
+    bless( $Self, $Type );
+
+    return $Self;
+}
+
+sub Run {
+    my ( $Self, %Param ) = @_;
+
+    my $ParamObject            = $Kernel::OM->Get('Kernel::System::Web::Request');
+    my $LayoutObject           = $Kernel::OM->Get('Kernel::Output::HTML::Layout');
+    my $StandardTemplateObject = $Kernel::OM->Get('Kernel::System::StandardTemplate');
+    my $StdAttachmentObject    = $Kernel::OM->Get('Kernel::System::StdAttachment');
+
+    # challenge token check for write action
+    $LayoutObject->ChallengeTokenCheck();
+
+    my ( %GetParam, %Errors );
+    for my $Parameter (qw(ID)) {
+        $GetParam{$Parameter} = $ParamObject->GetParam( Param => $Parameter ) || '';
+    }
+
+    my %Data = $StandardTemplateObject->StandardTemplateGet(
+        ID => $GetParam{ID},
+    );
+
+    # check if a standard template exist with this name
+    my $NameExists = 1;
+
+    while( $NameExists ) {
+        $Data{Name} .= ' (Copy)';
+        $NameExists = $StandardTemplateObject->NameExistsCheck(
+            Name => $Data{Name},
+        );
+    }
+
+    my $TemplateID = $StandardTemplateObject->StandardTemplateAdd(
+        %Data,
+        UserID => $Self->{UserID},
+    );
+
+    # update group
+    if ( $TemplateID ) {
+        my %SelectedAttachmentData = $StdAttachmentObject->StdAttachmentStandardTemplateMemberList(
+            StandardTemplateID => $GetParam{ID},
+        );
+
+        my %AttachmentsAll = $StdAttachmentObject->StdAttachmentList();
+
+        # check all used attachments
+        for my $AttachmentID ( sort keys %AttachmentsAll ) {
+            my $Active = $SelectedAttachmentData{$AttachmentID} ? 1 : 0;
+
+            # set attachment to standard template relation
+            my $Success = $StdAttachmentObject->StdAttachmentStandardTemplateMemberAdd(
+                AttachmentID       => $AttachmentID,
+                StandardTemplateID => $TemplateID,
+                Active             => $Active,
+                UserID             => $Self->{UserID},
+            );
+        }
+    }
+
+    return $LayoutObject->Redirect(
+        OP => 'Action=AdminTemplate',
+    );
+}
+
+1;
---
+++ Kernel/Output/HTML/FilterElementPost/AdminTemplateCopy.pm
@@ -0,0 +1,63 @@
+# --
+# Copyright (C) 2017 Perl-Services.de, http://www.perl-services.de/
+# --
+# This software comes with ABSOLUTELY NO WARRANTY. For details, see
+# the enclosed file COPYING for license information (AGPL). If you
+# did not receive this file, see http://www.gnu.org/licenses/agpl.txt.
+# --
+
+package Kernel::Output::HTML::FilterElementPost::AdminTemplateCopy;
+
+use strict;
+use warnings;
+
+our @ObjectDependencies = qw(
+    Kernel::Output::HTML::Layout
+);
+
+sub new {
+    my ( $Type, %Param ) = @_;
+
+    # allocate new hash for object
+    my $Self = {};
+    bless( $Self, $Type );
+
+    return $Self;
+}
+
+sub Run {
+    my ( $Self, %Param ) = @_;
+
+    my $LayoutObject = $Kernel::OM->Get('Kernel::Output::HTML::Layout');
+
+    # get template name
+    my $Templatename = $Param{TemplateFile} || '';
+    return 1 if !$Templatename;
+    return 1 if !$Param{Templates}->{$Templatename};
+
+    my $Baselink = $LayoutObject->{Baselink};
+    my $Token    = $LayoutObject->{UserChallengeToken};
+    my $Title    = $LayoutObject->{LanguageObject}->Translate("Copy Template");
+
+    ${ $Param{Data} } =~ s{
+        <a \s class="TrashCan" \s href=".*?ID=(\d+) .*? </a>\K
+    }{
+        $Self->__Linkify( $Baselink, $1, $Title, $Token );
+    }exmsg;
+
+    return 1;
+}
+
+sub __Linkify {
+    my ($Self, $Baselink, $TemplateID, $Title, $Token ) = @_;
+
+    my $Link = qq~
+        | <a href="${Baselink}Action=AdminTemplateCopy&ID=$TemplateID&ChallengeToken=$Token" title="$Title">
+             <span class="InvisibleText">$Title</span> <i class="fa fa-copy"></i></a>
+    ~;
+
+    return $Link;
+}
+
+
+1;
